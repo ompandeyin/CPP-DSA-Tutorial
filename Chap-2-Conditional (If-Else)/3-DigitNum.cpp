5882fc7e35b6d234aa773389edc5f26a9aa7470f
Om Pandey <omompandey7@gmail.com>
2026-03-08 13:45:51 +0530

    Add program: 3 Digit Number

---
+++ Chap-2-Conditional (If-Else)/3-DigitNum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"enter x: ";
+	cin>>x;
+
+	if(x>99 and x<1000){
+		cout<<"This is three digit number"<<endl;
+	}
+	else{
+		cout<<"This is not three digit number"<<endl;
+
+	}
+
+}    
