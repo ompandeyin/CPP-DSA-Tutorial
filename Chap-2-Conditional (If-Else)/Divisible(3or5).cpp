bfa7d93a78082a74c6d047b2aa6506a56538b75c
Om Pandey <omompandey7@gmail.com>
2026-03-09 23:30:24 +0530

    Add program: Divisible by 3 or 5

---
+++ Chap-2-Conditional (If-Else)/Divisible(3or5).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"enter x: ";
+	cin>>x;
+
+	if(x%3==0 or x%5==0){
+		cout<<"This is divisible by 3 or 5"<<endl;
+	}
+	else{
+		cout<<"This is not divisible by 3 or 5"<<endl;
+
+	}
+
+}   
