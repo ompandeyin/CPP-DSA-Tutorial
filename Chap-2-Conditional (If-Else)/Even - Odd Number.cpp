5a7e3ffb396c76021e78879d1c9d5b02e5d17e0d
Om Pandey <omompandey7@gmail.com>
2026-03-09 23:31:31 +0530

    Add program: Even Odd Number

---
+++ Chap-2-Conditional (If-Else)/Even - Odd Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"enter x: ";
+	cin>>x;
+	if(x%2==0){
+		cout<<"This number is even"<<endl;
+	}
+	else{
+		cout<<"This number is odd"<<endl;
+	} 
+	cout<<"Wow";
+
+}
