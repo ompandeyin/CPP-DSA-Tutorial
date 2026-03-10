0605067650bc68c4bd5a6e471da3360c5b193a2b
Om Pandey <omompandey7@gmail.com>
2026-03-10 13:06:28 +0530

    Add program: First program

---
+++ Chap-2-Conditional (If-Else)/First program.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	cout<<"Hello PW"<<endl; cout<<"Hello CW";
+//}
+
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	cout<<"Hello PW";
+//	cout<<"\n"; 
+//	cout<<endl; 
+//	cout<<"Hello CW";
+//}
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Hello PW"<<endl<<"Hello CW";
+}
