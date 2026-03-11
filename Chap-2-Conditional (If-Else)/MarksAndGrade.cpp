11a5cb0de406edfec33951cee85d044c64d68a97
Om Pandey <omompandey7@gmail.com>
2026-03-11 12:35:58 +0530

    Add program: Marks and Grade

---
+++ Chap-2-Conditional (If-Else)/MarksAndGrade.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter marks : ";
+	cin>>n;
+//	if(n>=81 and n<=100){
+//		cout<<"Very Good";
+//	}
+//	
+//	if(n>=61 and n<=80){
+//		cout<<"Good";
+//	}
+//	
+//	if(n>=41 and n<=60){
+//		cout<<"Average";
+//	}
+//	
+//	if(n<=40){
+//		cout<<"Fail";
+//	}
+//}
+
+
+
+	//use of else
+	//else check first to that using input
+	
+	//
+	
+	if(n>=81 and n<=100){
+		cout<<"Very Good";
+	}
+	
+	else if(n>=61){
+		cout<<"Good";
+	}
+	
+	else if(n>=41){
+		cout<<"Average";
+	}
+	
+	else{
+		cout<<"Fail";
+	}
+}  
+ 
