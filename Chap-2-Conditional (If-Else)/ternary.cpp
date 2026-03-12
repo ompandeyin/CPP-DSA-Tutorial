06260097ccd81bfed4da1268607a60083f8d3ccc
Om Pandey <omompandey7@gmail.com>
2026-03-12 10:08:37 +0530

    Add program: Ternary Operator

---
+++ Chap-2-Conditional (If-Else)/ternary.cpp
@@ -0,0 +1,27 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+// 	int n;
+// 	cout<<"enter n: ";
+// 	cin>>n;
+	
+// 	//(condition) ? if true :  if false ;
+	
+// 	(n%2==0) ? cout<<"even" : cout<<"odd";
+// }
+
+
+//it is same as if-else statement;
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Number is: ";
+	cin>>n;
+
+	(n%2==0) ? cout<<"Even" : cout<<"Odd";
+}
