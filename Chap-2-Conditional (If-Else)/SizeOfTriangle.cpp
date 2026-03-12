4b125192254aee7794ad03afd81e81fbd7170379
Om Pandey <omompandey7@gmail.com>
2026-03-12 10:07:51 +0530

    Add program: Size Of Triangle

---
+++ Chap-2-Conditional (If-Else)/SizeOfTriangle.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+// 	int a,b,c;
+// 	cout<<"Enter 1st side: ";
+// 	cin>>a;
+// 	cout<<"Enter 2st side: ";
+// 	cin>>b;
+// 	cout<<"Enter 3rd side: ";
+// 	cin>>c;
+
+// 	if((a+b)>c and (b+c)>a and (c+a)>b){
+// 		cout<<"Valid Triangle"<<endl;
+// 	}
+// 	else{
+// 		cout<<"Not a Valid Triangle"<<endl;
+
+// 	}
+
+// }   
+
+
