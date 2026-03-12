9f6d07bd7460acf799f1979c74b4430975e71a91
Om Pandey <omompandey7@gmail.com>
2026-03-12 10:09:20 +0530

    Add program: Untitled practice

---
+++ Chap-2-Conditional (If-Else)/Untitled10.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// struct university
+// {
+// string name;
+// string department_name;
+// int lib_id;
+// int roll;	
+// };
+// int main(){
+// 	university u1;
+// 	cout<<"Enter name: ";
+// 	cin>>u1.name;
+// 	cout<<"Enter department name: ";
+// 	cin>>u1.department_name;
+// 	cout<<"Enter lib id: ";
+// 	cin>>u1.lib_id;
+// 	cout<<"Enter roll no: ";
+// 	cin>>u1.roll;
+	
+// 	cout<<"Enter name: "<<u1.name<<endl;
+// 	cout<<"Enter department name: "<<u1.department_name<<endl;
+// 	cout<<"Enter lib id: "<<u1.lib_id<<endl;
+// 	cout<<"Enter roll no: "<<u1.roll<<endl;
+// }
+
+
